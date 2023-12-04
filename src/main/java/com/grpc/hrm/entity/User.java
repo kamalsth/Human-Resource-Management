a84da038741986c2d4622ca902a2f9fca53b2d75
kamalsth <kamalshrestha5618@gmail.com>
2023-12-04 10:25:25 +0545

    role and user class added

--- src/main/java/com/grpc/hrm/entity/User.java
+++ src/main/java/com/grpc/hrm/entity/User.java
@@ -1,4 +1,57 @@
 package com.grpc.hrm.entity;
 
 public class User {
+    private String username;
+    private String password;
+    private String nam;
+    private String email;
+    private String phone;
+
+    public User(String username, String password, String nam, String email, String phone) {
+        this.username = username;
+        this.password = password;
+        this.nam = nam;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNam() {
+        return nam;
+    }
+
+    public void setNam(String nam) {
+        this.nam = nam;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
