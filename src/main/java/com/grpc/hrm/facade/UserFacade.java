36f2e230fe2496b5ece1aaf25c7068803b79fb84
kamalsth <kamalshrestha5618@gmail.com>
2024-01-17 23:49:53 +0545

    code optimized for login and register

--- src/main/java/com/grpc/hrm/facade/UserFacade.java
+++ src/main/java/com/grpc/hrm/facade/UserFacade.java
@@ -1,6 +1,5 @@
 package com.grpc.hrm.facade;
 
-import com.grpc.hrm.config.JwtTokenResponse;
 import com.grpc.hrm.config.MapperConfig;
 import com.grpc.hrm.dto.LoginDto;
 import com.grpc.hrm.model.User;
@@ -8,6 +7,8 @@ import com.grpc.hrm.service.UserService;
 import com.grpc.hrm.utils.ValidateUserLogin;
 import com.grpc.hrm.utils.ValidateUsersForRegister;
 import com.ks.proto.auth.LoginRequest;
+import com.ks.proto.auth.LoginResponse;
+import com.ks.proto.common.StatusResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,16 +19,21 @@ public class UserFacade {
         this.userService = userService;
     }
 
-    public void register(com.ks.proto.user.User user) {
+    public StatusResponse register(com.ks.proto.user.User user) {
         ValidateUsersForRegister.validateUsersForRegister(user);
         User user1 = MapperConfig.INSTANCE.mapToUser(user);
         userService.register(user1);
+        return StatusResponse.newBuilder()
+                .setStatus("User registered successfully!!")
+                .build();
     }
 
-    public JwtTokenResponse login(LoginRequest loginRequest) {
+    public LoginResponse login(LoginRequest loginRequest) {
         ValidateUserLogin.validateUserLogin(loginRequest);
         LoginDto loginDto = MapperConfig.INSTANCE.mapToLoginDto(loginRequest);
-        return userService.login(loginDto);
+        return  LoginResponse.newBuilder()
+                .setToken(userService.login(loginDto).getToken())
+                .build();
     }
 
 
