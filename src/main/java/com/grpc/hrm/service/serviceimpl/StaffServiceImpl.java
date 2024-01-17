7c678ce39b1cfbb9417e3d4ed7348b7cd68d3bf0
kamalsth <kamalshrestha5618@gmail.com>
2024-01-18 00:07:42 +0545

    tax calculation for married and unmarried done

--- src/main/java/com/grpc/hrm/service/serviceimpl/StaffServiceImpl.java
+++ src/main/java/com/grpc/hrm/service/serviceimpl/StaffServiceImpl.java
@@ -1,11 +1,13 @@
 package com.grpc.hrm.service.serviceimpl;
 
+import com.grpc.hrm.model.MaritalStatus;
 import com.grpc.hrm.model.Staff;
 import com.grpc.hrm.model.User;
 import com.grpc.hrm.repository.StaffRepository;
 import com.grpc.hrm.repository.UserRepository;
 import com.grpc.hrm.service.StaffService;
 import com.grpc.hrm.utils.GenerateUUID;
+import com.grpc.hrm.utils.TaxCalculation;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -93,30 +95,12 @@ public class StaffServiceImpl implements StaffService {
         if (staff == null) {
             throw new RuntimeException("Staff not found for staff id : " + staffId);
         }
-        return taxCalculationPerYear(staff);
-
-    }
-
-    public double taxCalculationPerYear(Staff staff) {
-        double totalSalary = staff.getSalary() * 12;
-        double tax = 0.0;
-
-        if (totalSalary <= 500000) {
-            tax = totalSalary * 0.01;
-        } else if (totalSalary <= 700000) {
-            // 5,00,000 * 0.01 + (totalSalary - 5,00,000) * 0.1
-            tax = 5000 + (totalSalary - 500000) * 0.1;
-        } else if (totalSalary <= 1000000) {
-            // 5,00,000 * 0.01 + 2,00,000 * 0.1 + (totalSalary - 7,00,000) * 0.2
-            tax = 5000 + 20000 + (totalSalary - 700000) * 0.2;
-        } else if (totalSalary <= 2000000) {
-            // 5,00,000 * 0.01 + 2,00,000 * 0.1 + 3,00,000 * 0.2 + (totalSalary - 10,00,000) * 0.3
-            tax = 5000 + 20000 + 60000 + (totalSalary - 1000000) * 0.3;
-        } else {
-            // 5,00,000 * 0.01 + 2,00,000 * 0.1 + 3,00,000 * 0.2 + 10,00,000 * 0.3 + (totalSalary - 20,00,000) * 0.36
-            tax = 5000 + 20000 + 60000 + 300000 + (totalSalary - 2000000) * 0.36;
+        if (staff.getMaritalStatus().equals(MaritalStatus.MARRIED)) {
+            return TaxCalculation.taxCalculationPerYearForMarried(staff);
         }
-        return tax;
+        return TaxCalculation.taxCalculationPerYearForUnmarried(staff);
+
     }
+
 
 }
