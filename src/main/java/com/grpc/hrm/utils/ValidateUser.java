3a6c0363b69ea24a1aa4c2c9edf1d7bbbc147848
kamalsth <kamalshrestha5618@gmail.com>
2024-03-29 23:56:12 +0545

    validation added for each field during registration

--- src/main/java/com/grpc/hrm/utils/ValidateUser.java
+++ src/main/java/com/grpc/hrm/utils/ValidateUser.java
@@ -10,6 +10,7 @@ public class ValidateUser {
     public static void validationToChangePassword(ChangePasswordRequest request) {
         if (request.getOldPassword().isEmpty()) {
             throw new IllegalArgumentException("Old password field should not be empty");
+
         } else if (request.getNewPassword().isEmpty()) {
             throw new IllegalArgumentException("New password field should not be empty");
         }
