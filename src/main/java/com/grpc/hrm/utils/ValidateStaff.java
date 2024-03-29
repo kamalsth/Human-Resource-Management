9190b5083ab6ba43d4b78f2ef5e8cce1f7e26c26
kamalsth <kamalshrestha5618@gmail.com>
2024-03-30 00:00:33 +0545

    staff validation added for each field during

--- src/main/java/com/grpc/hrm/utils/ValidateStaff.java
+++ src/main/java/com/grpc/hrm/utils/ValidateStaff.java
@@ -2,15 +2,38 @@ package com.grpc.hrm.utils;
 
 public class ValidateStaff {
 
-    public static void validateStaff(com.ks.proto.staff.Staff staff){
-        if(staff.getName().isEmpty() || staff.getPersonalPhone().isEmpty() || staff.getEmergencyContactNumber().isEmpty() || staff.getPosition().isEmpty() || staff.getJoinDate()==0  || staff.getSalary()==0 || staff.getMaritalStatus().toString().isEmpty() || staff.getEmail().isEmpty()){
-            throw new IllegalArgumentException("Fields should not be empty");
+    public static void validateStaff(com.ks.proto.staff.Staff staff) {
+        if (staff.getName().isEmpty()) {
+            throw new IllegalArgumentException("Name should not be empty");
+
+        } else if (staff.getPersonalPhone().isEmpty()) {
+            throw new IllegalArgumentException("Personal Phone should not be empty");
+
+        } else if (staff.getEmergencyContactNumber().isEmpty()) {
+            throw new IllegalArgumentException("Emergency Contact Number should not be empty");
+
+        } else if (staff.getPosition().isEmpty()) {
+            throw new IllegalArgumentException("Position should not be empty");
+
+        } else if (staff.getJoinDate() == 0) {
+            throw new IllegalArgumentException("Join Date should not be empty");
+
+        } else if (staff.getSalary() == 0) {
+            throw new IllegalArgumentException("Salary should not be empty");
+
+        } else if (staff.getMaritalStatus().toString().isEmpty()) {
+            throw new IllegalArgumentException("Marital Status should not be empty");
+
+        } else if (staff.getEmail().isEmpty()) {
+            throw new IllegalArgumentException("Email should not be empty");
+
         }
+
     }
 
 
     public static void validateId(String staffId) {
-        if(staffId.isEmpty()){
+        if (staffId.isEmpty()) {
             throw new IllegalArgumentException("staffId should not be empty");
         }
     }
